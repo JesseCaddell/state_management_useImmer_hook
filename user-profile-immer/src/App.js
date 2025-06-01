2455253e5cf100a41bd8bb0a66504e233f1f3ea5
student <student@W-N-AppDev20.local>
2025-05-31 18:08:16 -0700

    Initial commit

---
+++ user-profile-immer/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserProfileWithImmer from './components/UserProfileWithImmer';
+
+function App() {
+  return (
+      <div className="App">
+        <UserProfileWithImmer />
+      </div>
+  );
+}
+
+export default App;
+
